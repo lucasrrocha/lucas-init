901470938b8ce2e17c24a8ba9fb80d6cce609952
Lucas Rodrigues Da Rocha <lucasrocha@brq.com>
2019-09-13 22:07:11 -0300

    update_header

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,9 @@
-// //console.log(test);
-// var test = document.querySelectorAll('li');
-// console.log(test);
-
-// (function(){
-//     console.log(test);
-//     for(var i = 0; i < test.length; i++){
-//         console.log(test[i]);
-//     }
-// })();
-
-
-document.addEventListener('DOMContentLoaded', function(event){
-    let test = document.querySelectorAll('.link');
-    test.forEach(item => {
-        console.log(item.textContent);
-        //item.style.color = 'blue';
-        item.textContent += ' Valor';
-    })
+// document.addEventListener('DOMContentLoaded', function(event){
+//     let test = document.querySelectorAll('.link');
+//     test.forEach(item => {
+//         console.log(item.textContent);
+//         //item.style.color = 'blue';
+//         item.textContent += ' Valor';
+//     })
     
-});
+// });
